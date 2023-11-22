1ce26b11882664be9e198b8e57ad997b045edb82
minhuw <wangmh15@gmail.com>
2023-11-22 16:38:57 +0800

    feat(feature): add devenv feature

---
+++ features/ubuntu-devenv/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive && sudo apt-get update && sudo apt-get install --no-install-recommends -y locales curl xz-utils ca-certificates python3 python3-pip && sudo apt-get clean && sudo rm -rf /var/lib/apt/lists/*
+
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --extra-conf "sandbox = true" --init none --no-confirm 
+
+cachix use devenv
+
+nix-env -if https://install.devenv.sh/python-rewrite
