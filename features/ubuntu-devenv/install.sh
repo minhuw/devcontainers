ed3129e1461bcfc2cf4636d33262f0bd7efc1e73
minhuw <wangmh15@gmail.com>
2023-11-24 17:37:57 +0800

    fix(nix): add systemd to use multi-user installation of nix

--- features/ubuntu-devenv/install.sh
+++ features/ubuntu-devenv/install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-export DEBIAN_FRONTEND=noninteractive && sudo apt-get update && sudo apt-get install --no-install-recommends -y locales curl xz-utils ca-certificates python3 python3-pip && sudo apt-get clean && sudo rm -rf /var/lib/apt/lists/*
+export DEBIAN_FRONTEND=noninteractive && sudo apt-get update && sudo apt-get install --no-install-recommends -y locales curl systemd xz-utils ca-certificates python3 python3-pip && sudo apt-get clean && sudo rm -rf /var/lib/apt/lists/*
 
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --extra-conf "sandbox = true" --init none --no-confirm 
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --extra-conf "sandbox = true" --no-start-daemon --no-confirm 
 
-sudo -i nix-env -i cachix
+ENV PATH="${PATH}:/nix/var/nix/profiles/default/bin"
+
+nix-env -i cachix
 
 cachix use devenv
 
-sudo -i nix-env -if https://install.devenv.sh/python-rewrite
+nix-env -if https://install.devenv.sh/python-rewrite
