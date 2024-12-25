cd0707c8747c218f4a9c3d8cacf8976df74100ca
minhuw <wangmh15@gmail.com>
2024-12-25 06:05:50 +0000

    feat(omnet++): add a OMNet++ devcontainer

--- features/ubuntu-nix/install.sh
+++ features/ubuntu-nix/install.sh
@@ -3,3 +3,5 @@
 export DEBIAN_FRONTEND=noninteractive && sudo apt-get update && sudo apt-get install --no-install-recommends -y locales curl xz-utils ca-certificates python3 python3-pip && sudo apt-get clean && sudo rm -rf /var/lib/apt/lists/*
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install linux --extra-conf "sandbox = true" --init none --no-confirm 
+
+nix profile install nixpkgs#fish nixpkgs#starship
